ddc53e569c11ae2fbc9fc30dc0c0fa418f7e80e6
Electron <electron@gmail.com>
2024-04-17 20:54:42 +0530

    feat: task models

---
+++ lib/models/task_model.dart/task_model.freezed.dart
@@ -0,0 +1,437 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'task_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+TaskModel _$TaskModelFromJson(Map<String, dynamic> json) {
+  return _TaskModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$TaskModel {
+//
+  String get ownerId =>
+      throw _privateConstructorUsedError; // where the task has to be performed or the assigned
+// user has to be report when the task is completed.
+  String? get locationId => throw _privateConstructorUsedError;
+  List<String> get relevantCategories => throw _privateConstructorUsedError;
+  DateTime? get milestoneTime => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  Gender? get gender => throw _privateConstructorUsedError;
+  int? get age => throw _privateConstructorUsedError;
+  double? get expectedPrice => throw _privateConstructorUsedError;
+  TaskStatus get status =>
+      throw _privateConstructorUsedError; // id stays an empty string when a new task is created
+// id will be assigned by the server.
+  String get id => throw _privateConstructorUsedError;
+  String? get assigned => throw _privateConstructorUsedError;
+  DateTime get createdAt => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $TaskModelCopyWith<TaskModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TaskModelCopyWith<$Res> {
+  factory $TaskModelCopyWith(TaskModel value, $Res Function(TaskModel) then) =
+      _$TaskModelCopyWithImpl<$Res, TaskModel>;
+  @useResult
+  $Res call(
+      {String ownerId,
+      String? locationId,
+      List<String> relevantCategories,
+      DateTime? milestoneTime,
+      String title,
+      String description,
+      Gender? gender,
+      int? age,
+      double? expectedPrice,
+      TaskStatus status,
+      String id,
+      String? assigned,
+      DateTime createdAt});
+}
+
+/// @nodoc
+class _$TaskModelCopyWithImpl<$Res, $Val extends TaskModel>
+    implements $TaskModelCopyWith<$Res> {
+  _$TaskModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? ownerId = null,
+    Object? locationId = freezed,
+    Object? relevantCategories = null,
+    Object? milestoneTime = freezed,
+    Object? title = null,
+    Object? description = null,
+    Object? gender = freezed,
+    Object? age = freezed,
+    Object? expectedPrice = freezed,
+    Object? status = null,
+    Object? id = null,
+    Object? assigned = freezed,
+    Object? createdAt = null,
+  }) {
+    return _then(_value.copyWith(
+      ownerId: null == ownerId
+          ? _value.ownerId
+          : ownerId // ignore: cast_nullable_to_non_nullable
+              as String,
+      locationId: freezed == locationId
+          ? _value.locationId
+          : locationId // ignore: cast_nullable_to_non_nullable
+              as String?,
+      relevantCategories: null == relevantCategories
+          ? _value.relevantCategories
+          : relevantCategories // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      milestoneTime: freezed == milestoneTime
+          ? _value.milestoneTime
+          : milestoneTime // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      gender: freezed == gender
+          ? _value.gender
+          : gender // ignore: cast_nullable_to_non_nullable
+              as Gender?,
+      age: freezed == age
+          ? _value.age
+          : age // ignore: cast_nullable_to_non_nullable
+              as int?,
+      expectedPrice: freezed == expectedPrice
+          ? _value.expectedPrice
+          : expectedPrice // ignore: cast_nullable_to_non_nullable
+              as double?,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as TaskStatus,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      assigned: freezed == assigned
+          ? _value.assigned
+          : assigned // ignore: cast_nullable_to_non_nullable
+              as String?,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$TaskModelImplCopyWith<$Res>
+    implements $TaskModelCopyWith<$Res> {
+  factory _$$TaskModelImplCopyWith(
+          _$TaskModelImpl value, $Res Function(_$TaskModelImpl) then) =
+      __$$TaskModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String ownerId,
+      String? locationId,
+      List<String> relevantCategories,
+      DateTime? milestoneTime,
+      String title,
+      String description,
+      Gender? gender,
+      int? age,
+      double? expectedPrice,
+      TaskStatus status,
+      String id,
+      String? assigned,
+      DateTime createdAt});
+}
+
+/// @nodoc
+class __$$TaskModelImplCopyWithImpl<$Res>
+    extends _$TaskModelCopyWithImpl<$Res, _$TaskModelImpl>
+    implements _$$TaskModelImplCopyWith<$Res> {
+  __$$TaskModelImplCopyWithImpl(
+      _$TaskModelImpl _value, $Res Function(_$TaskModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? ownerId = null,
+    Object? locationId = freezed,
+    Object? relevantCategories = null,
+    Object? milestoneTime = freezed,
+    Object? title = null,
+    Object? description = null,
+    Object? gender = freezed,
+    Object? age = freezed,
+    Object? expectedPrice = freezed,
+    Object? status = null,
+    Object? id = null,
+    Object? assigned = freezed,
+    Object? createdAt = null,
+  }) {
+    return _then(_$TaskModelImpl(
+      ownerId: null == ownerId
+          ? _value.ownerId
+          : ownerId // ignore: cast_nullable_to_non_nullable
+              as String,
+      locationId: freezed == locationId
+          ? _value.locationId
+          : locationId // ignore: cast_nullable_to_non_nullable
+              as String?,
+      relevantCategories: null == relevantCategories
+          ? _value._relevantCategories
+          : relevantCategories // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      milestoneTime: freezed == milestoneTime
+          ? _value.milestoneTime
+          : milestoneTime // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      gender: freezed == gender
+          ? _value.gender
+          : gender // ignore: cast_nullable_to_non_nullable
+              as Gender?,
+      age: freezed == age
+          ? _value.age
+          : age // ignore: cast_nullable_to_non_nullable
+              as int?,
+      expectedPrice: freezed == expectedPrice
+          ? _value.expectedPrice
+          : expectedPrice // ignore: cast_nullable_to_non_nullable
+              as double?,
+      status: null == status
+          ? _value.status
+          : status // ignore: cast_nullable_to_non_nullable
+              as TaskStatus,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      assigned: freezed == assigned
+          ? _value.assigned
+          : assigned // ignore: cast_nullable_to_non_nullable
+              as String?,
+      createdAt: null == createdAt
+          ? _value.createdAt
+          : createdAt // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$TaskModelImpl implements _TaskModel {
+  const _$TaskModelImpl(
+      {required this.ownerId,
+      this.locationId,
+      required final List<String> relevantCategories,
+      this.milestoneTime,
+      required this.title,
+      required this.description,
+      this.gender,
+      this.age,
+      this.expectedPrice,
+      this.status = TaskStatus.unassigned,
+      this.id = '',
+      this.assigned,
+      required this.createdAt})
+      : _relevantCategories = relevantCategories;
+
+  factory _$TaskModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$TaskModelImplFromJson(json);
+
+//
+  @override
+  final String ownerId;
+// where the task has to be performed or the assigned
+// user has to be report when the task is completed.
+  @override
+  final String? locationId;
+  final List<String> _relevantCategories;
+  @override
+  List<String> get relevantCategories {
+    if (_relevantCategories is EqualUnmodifiableListView)
+      return _relevantCategories;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_relevantCategories);
+  }
+
+  @override
+  final DateTime? milestoneTime;
+  @override
+  final String title;
+  @override
+  final String description;
+  @override
+  final Gender? gender;
+  @override
+  final int? age;
+  @override
+  final double? expectedPrice;
+  @override
+  @JsonKey()
+  final TaskStatus status;
+// id stays an empty string when a new task is created
+// id will be assigned by the server.
+  @override
+  @JsonKey()
+  final String id;
+  @override
+  final String? assigned;
+  @override
+  final DateTime createdAt;
+
+  @override
+  String toString() {
+    return 'TaskModel(ownerId: $ownerId, locationId: $locationId, relevantCategories: $relevantCategories, milestoneTime: $milestoneTime, title: $title, description: $description, gender: $gender, age: $age, expectedPrice: $expectedPrice, status: $status, id: $id, assigned: $assigned, createdAt: $createdAt)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$TaskModelImpl &&
+            (identical(other.ownerId, ownerId) || other.ownerId == ownerId) &&
+            (identical(other.locationId, locationId) ||
+                other.locationId == locationId) &&
+            const DeepCollectionEquality()
+                .equals(other._relevantCategories, _relevantCategories) &&
+            (identical(other.milestoneTime, milestoneTime) ||
+                other.milestoneTime == milestoneTime) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.gender, gender) || other.gender == gender) &&
+            (identical(other.age, age) || other.age == age) &&
+            (identical(other.expectedPrice, expectedPrice) ||
+                other.expectedPrice == expectedPrice) &&
+            (identical(other.status, status) || other.status == status) &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.assigned, assigned) ||
+                other.assigned == assigned) &&
+            (identical(other.createdAt, createdAt) ||
+                other.createdAt == createdAt));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      ownerId,
+      locationId,
+      const DeepCollectionEquality().hash(_relevantCategories),
+      milestoneTime,
+      title,
+      description,
+      gender,
+      age,
+      expectedPrice,
+      status,
+      id,
+      assigned,
+      createdAt);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$TaskModelImplCopyWith<_$TaskModelImpl> get copyWith =>
+      __$$TaskModelImplCopyWithImpl<_$TaskModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$TaskModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _TaskModel implements TaskModel {
+  const factory _TaskModel(
+      {required final String ownerId,
+      final String? locationId,
+      required final List<String> relevantCategories,
+      final DateTime? milestoneTime,
+      required final String title,
+      required final String description,
+      final Gender? gender,
+      final int? age,
+      final double? expectedPrice,
+      final TaskStatus status,
+      final String id,
+      final String? assigned,
+      required final DateTime createdAt}) = _$TaskModelImpl;
+
+  factory _TaskModel.fromJson(Map<String, dynamic> json) =
+      _$TaskModelImpl.fromJson;
+
+  @override //
+  String get ownerId;
+  @override // where the task has to be performed or the assigned
+// user has to be report when the task is completed.
+  String? get locationId;
+  @override
+  List<String> get relevantCategories;
+  @override
+  DateTime? get milestoneTime;
+  @override
+  String get title;
+  @override
+  String get description;
+  @override
+  Gender? get gender;
+  @override
+  int? get age;
+  @override
+  double? get expectedPrice;
+  @override
+  TaskStatus get status;
+  @override // id stays an empty string when a new task is created
+// id will be assigned by the server.
+  String get id;
+  @override
+  String? get assigned;
+  @override
+  DateTime get createdAt;
+  @override
+  @JsonKey(ignore: true)
+  _$$TaskModelImplCopyWith<_$TaskModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
