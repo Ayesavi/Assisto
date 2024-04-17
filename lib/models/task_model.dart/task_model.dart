57b43fda235b13092ec41ad472fc54f26db9fba3
Electron <electron@gmail.com>
2024-04-17 20:55:37 +0530

    KAN-6 fix

--- lib/models/task_model.dart/task_model.dart
+++ lib/models/task_model.dart/task_model.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'task_model.g.dart';
 
 part 'task_model.freezed.dart';
-
+ 
 @freezed
 class TaskModel with _$TaskModel {
   const factory TaskModel({
