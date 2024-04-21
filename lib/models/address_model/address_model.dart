fa6983c0f7a2f995f5762e5e542fb13815cc9e99
Electron <electron@gmail.com>
2024-04-21 13:48:58 +0530

    KAN-16 added models and repos

---
+++ lib/models/address_model/address_model.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: invalid_annotation_target
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'address_model.freezed.dart';
+part 'address_model.g.dart';
+
+@freezed
+class AddressModel with _$AddressModel {
+  factory AddressModel({
+    required String address,
+    String? landmark,
+    required ({double lat,   double lng}) latlng,
+    @JsonKey(name: 'owner_id',includeToJson: false)
+    String? ownerId,
+    @JsonKey(name: 'created_at')
+
+    required DateTime createdAt,
+    @JsonKey(includeToJson: false)
+    required int id,
+  }) = _AddressModel;
+
+  factory AddressModel.fromJson(Map<String, dynamic> json) =>
+      _$AddressModelFromJson(json);
+}
+
+
+
+extension ToSupa on AddressModel {
+  Map<String, dynamic> toSupaJson() {
+    final json = toJson();
+    json['latlng'] = 'POINT(${json['latlng']['lng']} ${json['latlng']['lat']})';
+    return json;
+  }
+}
